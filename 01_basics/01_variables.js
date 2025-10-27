2f046a9b5f785960f72d7d448c6a4767ead66fa9
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-27 17:19:15 +0530

    code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = "shivam99@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kanpur";
+
+// accountId = 2; // not allowed
+console.log(accountId);
+
+accountEmail = "shivamrajput99@gmail.com"; // allow
+console.log(accountEmail);
