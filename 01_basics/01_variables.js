ba0c7ce4aacf905fb92618075de4b45ddbe2ad9a
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-27 18:35:49 +0530

    code

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,21 @@ const accountId = 144553;
 let accountEmail = "shivam99@gmail.com";
 var accountPassword = "12345";
 accountCity = "Kanpur";
+let accountState;
 
 // accountId = 2; // not allowed
 console.log(accountId);
 
 accountEmail = "shivamrajput99@gmail.com"; // allow
 console.log(accountEmail);
+
+accountCity = "Kannauj";
+console.log(accountCity);
+
+console.log(accountState);    // output is --> undefined
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity]);
