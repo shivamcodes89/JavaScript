927e308ec2485130b40f8f55911a1e82cd0044e4
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-27 20:25:50 +0530

    code

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 3)   // code readability should be high
+
+
+console.log("Shivam");
+
+
+let name = "Shivam";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// String 
+// boolean => true/false
+// null => standalone value
+// undefind => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Shivam")   // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
