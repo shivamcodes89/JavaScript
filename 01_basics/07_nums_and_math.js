7a82cf3222f525f533cdc24d060f7be0308c5c37
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-04 06:51:33 +0530

    code

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,36 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber1 = 23.8966;
+// console.log(otherNumber1.toPrecision(3));
+// const otherNumber2 = 123.8966;
+// console.log(otherNumber2.toPrecision(3));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// >>>>>>>>>>>>>>>>>>> MAths >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.3)); // 4
+// console.log(Math.round(4.6)); // 5
+
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.9)); // 4
+
+// console.log(Math.min(3, 9, 6, 7, 4));
+// console.log(Math.max(3, 9, 6, 7, 4));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
