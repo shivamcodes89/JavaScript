225cdce037016f17f6a3c7c5ad6c96796fbd2623
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-06 22:10:48 +0530

    code

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -33,4 +33,8 @@
 // console.log(Math.max(3, 9, 6, 7, 4));
 
 console.log(Math.random());
-console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
