9a5b6f255c04ec5503519d51af12a8d036caf3e7
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-07 17:47:55 +0530

    code

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,26 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreateDate = new Date(2025, 0, 23);
+// let myCreateDate = new Date(2023, 0, 23, 5, 3);
+// let myCreateDate = new Date("2023-01-14");
+let myCreateDate = new Date("01-14-2023");
+console.log(myCreateDate.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myCreateDate);
+console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
