0dc8132d59f104d839b331f6f26e7192a93d1b92
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-08 17:23:45 +0530

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,26 @@
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+// // console.log(a);
+// // console.log(b);
+// console.log(c); // ye 30 print karega jab ki isko scope ke bahar nahi aana chahiye 
+//                 // islilye var ki jagah let use karege
+
+// var c = 500;
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+// console.log(c); // 500 print hona chahiye lekin 30 print ho raha
+
+
+let a = 300
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+console.log(a);
