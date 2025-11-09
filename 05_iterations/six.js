70e54f7d75dc8372d622626665ba1b2264f23aa6
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-10 02:01:25 +0530

    code

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -14,7 +14,34 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNums.filter((num) => num > 5)
 // console.log(newNums);
-const newNums = myNums.filter((num) => {
-    num > 5
-})
-console.log(newNums);
+
+// const newNums = myNums.filter((num) => {
+//     return num > 5
+// })
+// console.log(newNums);    // agar scope open karte hai to return likhna padega 
+
+// const newNums = []
+// myNums.forEach((num) => {
+//     if(num > 5){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
+const books = [
+    {title: "Book One", genre: "Fiction", publish: 1981, edition: 2004},
+    {title: "Book two", genre: "Non-Fiction", publish: 1992, edition: 2008},
+    {title: "Book three", genre: "History", publish: 1999, edition: 2007},
+    {title: "Book four", genre: "Non-Fiction", publish: 1989, edition: 2010},
+    {title: "Book five", genre: "Science", publish: 2009, edition: 2014},
+    {title: "Book six", genre: "Fiction", publish: 1987, edition: 2010},
+    {title: "Book seven", genre: "History", publish: 1981, edition: 1996},
+    {title: "Book eight", genre: "Science", publish: 2011, edition: 2016},
+    {title: "Book nine",genre: "Non-Fiction", publish: 1981, edition: 1986},
+]
+// const userBooks = books.filter((bk) => bk.genre === "History")
+//const userBooks = books.filter((bk) => {return bk.publish >= 1995})
+const userBooks = books.filter((bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"})
+console.log(userBooks);
