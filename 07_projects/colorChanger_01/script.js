99db33aaaa494acb84e855cb02755bfef41ae5f0
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-12-28 08:32:19 +0530

    CODE

---
+++ 07_projects/colorChanger_01/script.js
@@ -0,0 +1,11 @@
+const red = document.querySelector(".red");
+const orange = document.querySelector(".orange");
+const yellow = document.querySelector(".yellow");
+const green = document.querySelector(".green");
+const blue = document.querySelector(".blue");
+
+red.onclick = () => document.body.style.background = "#ff4b4b";
+orange.onclick = () => document.body.style.background = "#ff9c3b";
+yellow.onclick = () => document.body.style.background = "#ffd100";
+green.onclick = () => document.body.style.background = "#30d9b4";
+blue.onclick = () => document.body.style.background = "#3569ff";
