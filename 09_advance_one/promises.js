8d94c02e8531fcbf127be60f5729d37c5d020618
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-02-09 20:33:52 +0530

    promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,13 @@
+// const promiseOne = new Promise(function(resolve, reject){
+//   // Do an async task
+//   // DB calls, cryptography, network
+//   setTimeout(function(){
+//     console.log("Async task is complete");
+//     resolve()
+//   }, 10000) 
+// })
+
+// promiseOne.then(function(){
+//   console.log("promise consumed");
+// })
+
