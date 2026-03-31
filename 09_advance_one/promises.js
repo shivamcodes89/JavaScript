720e35de5c1a586c29b045c58fe4ed812a07ebaf
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-03-31 07:38:56 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -11,3 +11,25 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
   console.log("promise consumed");
 })
+
+new Promise(function(resolve, reject){
+  setTimeout(function(){
+    console.log("Async task 2")
+    resolve();
+  }, 1000)
+})
+.then(function(){
+  console.log("Async 2 resolve");
+})
+
+
+const promiseThree = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve({username: "shivamcodes", emial: "shivam@676.com"})
+  }, 1000)
+})
+
+promiseThree.then(function(user){
+  console.log(user);
+  
+})
