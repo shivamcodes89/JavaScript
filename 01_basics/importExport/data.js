f1baaed394afaec1fa73c46a9ed5c942d7525064
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-04-06 18:35:14 +0530

    importExport

---
+++ 01_basics/importExport/data.js
@@ -0,0 +1,19 @@
+// const num = {
+//   name: "shivam",
+//   age: "20",
+//   course: "btech"
+// };
+// module.export = num;
+
+
+const user = "shivam";
+module.exports = user;
+
+
+// const user = {
+//   name: "shivam",
+//   age: "20",
+//   course: "btech",
+//   year: "2nd"
+// }
+// module.exports = user; 
---
+++ 01_basics/importExport/print.js
@@ -0,0 +1,12 @@
+// const data = require("./data.js");
+// console.log(data.name);
+// console.log(data.course);
+
+
+const users = require("./data");
+console.log(users.user);
+
+
+// const users = require("./data.js");
+// console.log(users.year);
+// console.log(users.age);
