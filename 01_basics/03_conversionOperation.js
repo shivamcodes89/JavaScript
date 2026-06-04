8c7a511ee82b400644279580aa1c680610074e17
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-06-04 11:00:38 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,73 +1,37 @@
-// let score = "33abc"
-
-// console.log(typeof score); // string
-// console.log(typeof(score)); // string
-
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); // number  
-// console.log(valueInNumber); // NaN
-
-
-
-
-// let score = null
+// let score = "10abc";
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); // number
-// console.log(valueInNumber); // 0
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+// let score = null;
 
+// console.log(typeof score);
+// console.log(typeof(score));
 
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let score = undefined
+let score = undefined;
 
 console.log(typeof score);
 console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // NaN
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
-// "true" => 1; false => 0;
+// true => 1; false => 0;
 
+let isLoggedIn = 1;
+let booleanIsLoggeIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggeIn);
 
-// let isLoggedIn = 1
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn); // true
-// console.log(typeof booleanIsLoggedIn); // boolean
-
-// let isLoggedIn = ""
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn); // false
-// console.log(typeof booleanIsLoggedIn); // boolean
-
-let isLoggedIn = "Shivam"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); // true
-console.log(typeof booleanIsLoggedIn); // boolean
-
-
-// 1 => true; 0 => false
-// "" => false
-// "Shivam" => true
-
-
-// ******************************* OPERATION ****************************
-
-let value = 3
-let negValue = -value
-// console.log(negValue);   // -3
-
-// console.log(2 + 2); // 4
-// console.log(2 - 2); // 0
-// console.log(2 * 2);  // 4
-// console.log(2 ** 3); // 8  
-// console.log(2 / 3);  // 0.66666666666666
-// console.log(2 % 3);   // 2
-
+// 1=>true, 0=>false
